e6ae33142f724b3f63eeb0e0becdd085a21d9236
Kaan Gemalmazoğlu <n0syste3m65@hotmail.com>
2020-04-25 13:41:42 +0300

    First model and migrations

---
+++ MainMusicStore.Models/DbModels/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MainMusicStore.Models.DbModels
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Bu Alan Boş Geçilemez!")]
+        [StringLength(250,MinimumLength =10,ErrorMessage ="Şartlara uygun değer giriniz...")]
+        public string Name { get; set; }
+
+
+
+    }
+}
